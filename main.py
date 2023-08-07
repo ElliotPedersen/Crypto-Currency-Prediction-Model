488b2a782898594534ac236d47a70bb1b4f7012e
Elliot <elliot.pedersen@outlook.com>
2023-08-07 10:47:56 +0200

    Corrected some data and adding graph types

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import seaborn as sb
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from matplotlib.widgets import Button, CheckButtons
 from sklearn.svm import SVC
 from xgboost import XGBClassifier
 from sklearn import metrics
@@ -15,8 +16,9 @@ warnings.filterwarnings('ignore')
 
 df = pd.read_csv('bitcoin.csv')
 
-plt.figure(figsize=(15, 5))
+plt.figure(figsize=(15, 7))
 plt.plot(df['Close'])
 plt.title('Bitcoin price.', fontsize=15)
 plt.ylabel('Price in dollars.')
+
 plt.show()
